17d05ec9067583f655595c8a0dfd51dc4e11f9cd
XinSweet <skilore@163.com>
2016-03-12 16:42:03 +0800

    Initial Commit

---
+++ LoginDemo1.1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginDemo1.1
+//
+//  Created by 张祚新 on 16/3/12.
+//  Copyright © 2016年 张祚新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoginDemo1.1UITests/LoginDemo1_1UITests.m
@@ -0,0 +1,40 @@
+//
+//  LoginDemo1_1UITests.m
+//  LoginDemo1.1UITests
+//
+//  Created by 张祚新 on 16/3/12.
+//  Copyright © 2016年 张祚新. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LoginDemo1_1UITests : XCTestCase
+
+@end
+
+@implementation LoginDemo1_1UITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
